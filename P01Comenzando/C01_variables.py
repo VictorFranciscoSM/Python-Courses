2091828c90599d7f20d1370c4e2d9c035f8c7c49
VictorFranciscoSM <victorfran27@gmail.com>
2024-09-28 18:32:14 -0600

    if-else

--- P01Comenzando/C01_variables.py
+++ P01Comenzando/C01_variables.py
@@ -3,6 +3,17 @@ print("**********")
 print("Adivina el numero")
 print("**********")
 
+guess = 25
+valor = input("intriduce un numero")
+print("Tu numero fue: ", valor)
+
+if(guess == int(valor)):
+    print("Adivinaste")
+else:
+    print("Intentalo de nuevo")
+
 edad1 = 10
 edad2 = "20"
 print(edad1, edad2, edad1 + int(edad2))
+producto = edad1 * edad2
+print(producto)

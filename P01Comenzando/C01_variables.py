64cb5d090ea657b5463c92319f7b5f9a84cd76ae
VictorFranciscoSM <victorfran27@gmail.com>
2024-09-28 18:05:56 -0600

    concantenacion

---
+++ P01Comenzando/C01_variables.py
@@ -0,0 +1,8 @@
+#Para subirlo al repo se necesitan los comando de git
+print("**********")
+print("Adivina el numero")
+print("**********")
+
+edad1 = 10
+edad2 = "20"
+print(edad1, edad2, edad1 + int(edad2))

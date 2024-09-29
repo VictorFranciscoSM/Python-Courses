8fef93bb5999db3a04c57812f8e22b37e2fb4c9b
VictorFranciscoSM <victorfran27@gmail.com>
2024-09-29 13:22:22 -0600

    added elif (if, elif, else)

--- P01Comenzando/C02_adivinaElNumero.py
+++ P01Comenzando/C02_adivinaElNumero.py
@@ -9,10 +9,18 @@ valor_str = input("introduce un numero: ")  #sera un valor str
 valor_int = int(valor_str)
 print("Tu numero fue: ", valor_int)
 
+acierto = guess == valor_int
+mayor = valor_int > guess
+menor = valor_int < guess
+
 #La identacion en python es omportante ya que si no se usa, marca error
-if(guess == valor_int):
+if(acierto):
     print("Adivinaste")
 else:
+    if(mayor):
+        print("El numero es mayoy")
+    elif(menor):
+        print("El numero es menor")
     print("Intentalo de nuevo")
 
 print("El juego a concluido")

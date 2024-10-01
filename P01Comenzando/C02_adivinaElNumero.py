90fdce49430cd20f56f021a0c54adb836a9aec30
VictorFranciscoSM <victorfran27@gmail.com>
2024-10-01 17:41:47 -0600

    Use  of break and continue

--- P01Comenzando/C02_adivinaElNumero.py
+++ P01Comenzando/C02_adivinaElNumero.py
@@ -14,6 +14,11 @@ for intento in range(1,4):
     print(f"Intento {intento} de {total_intentos}")
     valor_str = input("introduce un numero: ")  #sera un valor str
     valor_int = int(valor_str)
+
+    if (valor_int <1 or valor_int >100):
+        print("Digita un nnumero mayor que 0 y menor que 100")
+        continue    #se salta a la siguiente iteration cuando entra, ya que no sirve que evalue lo siguiente
+
     print("Tu numero fue: ", valor_int)
 
     acierto = guess == valor_int
@@ -23,9 +28,10 @@ for intento in range(1,4):
     #La identacion en python es omportante ya que si no se usa, marca error
     if(acierto):
         print("Adivinaste")
+        break   #una vez que se ejectura se rompe el ciclo donde se encuentre
     else:
         if(mayor):
-            print("El numero es mayoy")
+            print("El numero es mayor")
         elif(menor):
             print("El numero es menor")
         print("Intentalo de nuevo")

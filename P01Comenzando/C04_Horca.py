3f0869238b1f06f6cb0bab23c50b6f733f200e2f
VictorFranciscoSM <victorfran27@gmail.com>
2024-10-03 16:02:45 -0600

    creation of game ahorcado

--- P01Comenzando/C04_Horca.py
+++ P01Comenzando/C04_Horca.py
@@ -1,9 +1,32 @@
 def jugar():
     print("***********************************")
-    print("***************Horca***************")
+    print("************El Ahorcado************")
     print("***********************************")
-    print("Elige el nivel de dificultad")
-    print("(1)Novato (2)Intermedio (3)Avanzado")
+    print("Biemvemido al juego el Ahorcado")
+
+    secretWord = "banana" 
+    ahorco = False
+    acerto = False
+    tamano = len(secretWord)
+    lista_vacia = [None]*tamano
+
+    i=0
+    longitud = 2
+    while (not ahorco and not acerto) and i < tamano:
+        while longitud > 1:
+            intento = input("Digita una letra: ")
+            longitud = len(intento)
+            if(longitud != 1):
+                print("Ingresa solo una letra")
+        indice = 0
+        for letra in secretWord:
+            print(letra)
+            if letra == intento:
+                print(f"Entoncre la letra '{letra}', en la posicion {indice}")
+                lista_vacia[indice] = letra
+            indice +=1
+        i += 1    
+
 
 if(__name__ == "__main__"):
     jugar()

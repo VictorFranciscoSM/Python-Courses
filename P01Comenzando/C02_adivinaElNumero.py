f3297537b55ecfca9231aeaac073c1d1c220f4c2
VictorFranciscoSM <victorfran27@gmail.com>
2024-10-03 11:10:49 -0600

    puntos agregados

--- P01Comenzando/C02_adivinaElNumero.py
+++ P01Comenzando/C02_adivinaElNumero.py
@@ -25,6 +25,7 @@ else:
 #guess = round(random.random() * 100)    #primero nos da unnuemro del 0 al 1 y despues la redondea
 guess = random.randint(1,100)
 print("El nuemro secreto es: ", guess)
+puntos = 1000
 
 for intento in range(1,total_intentos + 1):                       #range genera un rangp de numeros secuenciales
     #print("Intento {} de {}".format(intento, total_intentos))
@@ -33,7 +34,7 @@ for intento in range(1,total_intentos + 1):                       #range genera 
     valor_int = int(valor_str)
 
     if (valor_int <1 or valor_int >100):
-        print("Digita un nnumero mayor que 0 y menor que 100")
+        print("Digita un numero mayor que 0 y menor que 100")
         continue    #se salta a la siguiente iteration cuando entra, ya que no sirve que evalue lo siguiente
 
     print("Tu numero fue: ", valor_int)
@@ -45,6 +46,7 @@ for intento in range(1,total_intentos + 1):                       #range genera 
     #La identacion en python es omportante ya que si no se usa, marca error
     if(acierto):
         print("Adivinaste")
+        print(f"Has acertado el numero es {guess}, tu puntaje es {puntos}")
         break   #una vez que se ejectura se rompe el ciclo donde se encuentre
     else:
         if(mayor):
@@ -52,6 +54,8 @@ for intento in range(1,total_intentos + 1):                       #range genera 
         elif(menor):
             print("El numero es menor")
         print("Intentalo de nuevo")
+        puntosPerdidos = abs(valor_int - guess)
+        puntos = puntos - puntosPerdidos
 
 print("El juego a concluido")
 

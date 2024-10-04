1103e29ce304b791ebe44c4ca828058e4e06ce2b
VictorFranciscoSM <victorfran27@gmail.com>
2024-10-04 12:49:18 -0600

    use of list with in nad not in

--- P01Comenzando/C04_Horca.py
+++ P01Comenzando/C04_Horca.py
@@ -2,36 +2,44 @@ def jugar():
     print("***********************************")
     print("************El Ahorcado************")
     print("***********************************")
-    print("Biemvemido al juego el Ahorcado")
+    print("Biemvemido al juego el Ahorcado\n")
 
     secretWord = "banana" 
     secretWord = secretWord.strip()               #Elimana todos los espacios dentro del string
     ahorco = False
     acerto = False
     tamano = len(secretWord)
-    lista_vacia = [None]*tamano
+    lista_vacia = ["_"]*tamano                   #Multiplica la lista ["_"] por un cierto valor y regresa la lista con ese valor en los elemntos
+    print(lista_vacia)
 
     i=0
-    longitud = 2
     while (not ahorco and not acerto) and i < tamano:
+        longitud = 2
         while longitud > 1:
-            intento = input("Digita una letra: ")
+            intento = input("\nDigita una letra: ")
             intento = intento.strip()               #Elimina todos los espacios dentro del string
             #intento = intento.lower()               #Covierte todos los caracteres a minusculas
-            intento = intento.upper()               #Covierte todos los caracteres a mayusculas
+            #intento = intento.upper()               #Covierte todos los caracteres a mayusculas
             longitud = len(intento)
             if(longitud != 1):
                 print("Ingresa solo una letra")
 
         indice = 0
         for letra in secretWord:
-            print(letra)
-            if letra == intento:
-                print(f"Entoncre la letra '{letra}', en la posicion {indice}")
-                lista_vacia[indice] = letra
+            #print(letra)
+            if letra.upper() == intento.upper():
+                #print(f"Entoncre la letra '{letra}', en la posicion {indice}")
+                lista_vacia[indice] = letra.upper()
             indice +=1
-        i += 1    
-
+        print(lista_vacia)
+        i += 1  
+        if("_" not in lista_vacia):
+            i = len(secretWord)
+    if("_" in lista_vacia):
+        print("Perdiste")
+    else:
+        print("Has completado el juego")
+    print(lista_vacia)
 
 if(__name__ == "__main__"):
     jugar()

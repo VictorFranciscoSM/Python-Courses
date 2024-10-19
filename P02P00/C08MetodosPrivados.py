b6ee3891a4ffca20510e8f1b553a4a99ce026f0e
VictorFranciscoSM <victorfran27@gmail.com>
2024-10-19 12:06:24 -0600

    metodos privados

--- P02P00/C08MetodosPrivados.py
+++ P02P00/C08MetodosPrivados.py
@@ -12,7 +12,7 @@ class Cuenta:
     def deposita(self, valor):
         self.__saldo += valor
 
-    def __puede_retirar(self, valor_a_retirar):
+    def __puede_retirar(self, valor_a_retirar):                 #metodo privado ya que solo se usa dentro de la clase
         valor_disponible = self.__saldo + self.__limite
         return valor_a_retirar <= valor_disponible        
 
@@ -41,3 +41,16 @@ class Cuenta:
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+cuenta1 = Cuenta(123,'Victor',100.0,1000.0)
+cuenta2 = Cuenta(427,'Arturo',1200.0,6200.0)
+
+cuenta1.retira(1001.0)
+cuenta1.extracto()
+print(cuenta1.saldo)
+
+
+#Estos métodos se utilizan para encapsular la lógica interna de una 
+#clase y evitar que sean accedidos desde fuera. Para definir un 
+#método privado, simplemente se le antepone un doble guion bajo 
+#(__) al nombre del método.
